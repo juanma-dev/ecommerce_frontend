1dbef8d10cc81ef6f324502287f633cae990ba9b
juanma-dev <juanmanuelcastellanoshernandez@gmail.com>
2021-01-20 16:30:52 -0500

    Ecommerce FrontEnd App

---
+++ src/ecommerce.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+const Ecommerce = (props) => {
+  return (
+    <BrowserRouter>
+      <div>TEST</div>
+    </BrowserRouter>
+  );
+};
+
+export { Ecommerce as default };
